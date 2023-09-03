6cfddd485f4308ec5f92f4a3c3ecb6f80235fb12
Josh Pollock <jpollock412@gmail.com>
2023-09-03 12:50:46 -0400

    Add classifier function

--- workers/joshdata/src/classify.ts
+++ workers/joshdata/src/classify.ts
@@ -1,16 +1,5 @@
 type Where = 'starts' | 'ends' | 'contains' | 'exact';
-type Classification = {
-	// search(es) to run.
-  searches: {
-    search: string | string[];
-	  where: Where;
-		all: boolean;
-	}[];
-	// If all searches, or just one must match
-	all: boolean;
-	// classification name
-	name: string;
-}
+
 function searchString(subject: string, search: string | string[], where: Where, all = false): boolean {
   if (Array.isArray(search)) {
     if (all) {
@@ -32,4 +21,60 @@ function searchString(subject: string, search: string | string[], where: Where, 
         return false;
     }
   }
+}
+type Classification = {
+	// search(es) to run.
+  searches: {
+    search: string | string[];
+	  where: Where;
+		all: boolean;
+	}[];
+	// If all searches, or just one must match
+	all: boolean;
+	// classification id
+	id: string;
+};
+
+type Source = {
+  id: string;
+  text:source;
+};
+
+type Match = {
+  source: Source;
+  classifications: string[];
+}
+
+function classifySources(sources: Source[], classifications: Classification[]): Match[] {
+  const matches: Match[] = [];
+
+  // iterate over each source
+  sources.forEach(source => {
+    const matchedClassifications: string[] = [];
+
+    // iterate over each classification
+    classifications.forEach(classification => {
+      // check if all searches match the source text
+      const allSearchesMatch = classification.searches.every(search => {
+        return searchString(source.text, search.search, search.where, search.all);
+      });
+
+      // if all searches must match and they do, or if not all searches must match and at least one does,
+      // add the classification id to the matched classifications
+      if ((classification.all && allSearchesMatch) || (!classification.all && classification.searches.some(search => {
+        return searchString(source.text, search.search, search.where, search.all);
+      }))) {
+        matchedClassifications.push(classification.id);
+      }
+    });
+
+    // if there are any matched classifications, add a new match with the source and the matched classifications
+    if (matchedClassifications.length > 0) {
+      matches.push({
+        source: source,
+        classifications: matchedClassifications
+      });
+    }
+  });
+  return matches;
 }
