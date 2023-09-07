a5e1314914ec76b3b9130e709943297add7fa30f
Josh Pollock <jpollock412@gmail.com>
2023-09-06 21:55:14 -0400

    Fix KV import in test

--- workers/joshdata/src/dataApi.test.ts
+++ workers/joshdata/src/dataApi.test.ts
@@ -1,5 +1,13 @@
 import { dataApi } from './dataApi';
-import { KVNamespace } from 'edge-mock';
+import {EdgeKVNamespace as KVNamespace} from 'edge-mock';
+describe('edge-mock', () => {
+    test('put and get kv', async() => {
+        const kv = new KVNamespace();
+        await kv.put('foo','bar');
+        const value = await kv.get('foo');
+        expect(value).toBe('bar');
+    });
+});
 
 describe('dataApi', () => {
     let kv: KVNamespace;
