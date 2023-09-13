1921b5a9bcab4b8d759b7449441d93050502f588
josh <jpollock412@gmail.com>
2023-09-13 16:10:32 -0400

    Refactor worker to be likes or post and use basic auth

--- workers/tooter/src/index.ts
+++ workers/tooter/src/index.ts
@@ -1,34 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	KV: KVNamespace;
-	josh412bskypass: string;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
 }
-import  { AtpSessionData, AtpSessionEvent, BskyAgent } from '@atproto/api';
+import  { AtpSessionData, AtpSessionEvent, BskyAgent, RichText } from '@atproto/api';
 import { jsonResponse } from './utils';
-
-
 
 export async function login({ service, identifier, password,kv }: {
   service?: string,
@@ -37,12 +12,15 @@ export async function login({ service, identifier, password,kv }: {
   kv: KVNamespace,
 }) {
 
-	const saveKey = `atp-savedsession:${service}:${identifier}`;
+	const saveKey = `at-savedsession_2:${service}:${identifier}`;
 	const agent = new BskyAgent({
 		service: service ?? 'https://bsky.social',
 		persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
 			if( sess && ! ['created','updated'].includes(evt) ) {
-				kv.put(saveKey, JSON.stringify(sess));
+				kv.put(saveKey, JSON.stringify(sess), {
+					//expire in 1 hour
+					expirationTtl: 60 * 60,
+				});
 			}else{
 				kv.delete(saveKey);
 			}
@@ -72,32 +50,172 @@ export async function login({ service, identifier, password,kv }: {
 	return {agent,resumed};
 }
 
-type T_error = {
-	message: string,
-	headers?: Record<string,string>,
-}
+const ALLOWED_USERS = [
+	'josh412.com',
+	'josh412.dev'
+];
+/**
+     * Parse HTTP Basic Authorization value.
+     * @param {Request} request
+     * @throws {BadRequestException}
+     * @returns {{ user: string, pass: string }}
+     */
+async function basicAuthentication(request:Request) {
+	const Authorization = request.headers.get("Authorization") || '';
+
+	const [scheme, encoded] = Authorization.split(" ");
+
+	// The Authorization header must start with Basic, followed by a space.
+	if (!encoded || scheme !== "Basic") {
+		return  {
+			status: 400,
+			message: 'Malformed authorization header.',
+			statusText: 'Unauthorized',
+			user: null,
+			pass: null,
+		}
+	}
+
+
+	// Decodes the base64 value and performs unicode normalization.
+	// @see https://datatracker.ietf.org/doc/html/rfc7613#section-3.3.2 (and #section-4.2.2)
+	// @see https://dev.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+	const buffer = Uint8Array.from(atob(encoded), (character) =>
+	  character.charCodeAt(0)
+	);
+	const decoded = new TextDecoder().decode(buffer).normalize();
+
+	// The username & password are split by the first colon.
+	//=> example: "username:password"
+	const index = decoded.indexOf(":");
+
+	let user : string|null = null;
+	let pass : string|null = null;
+	// The user & password are split by the first colon and MUST NOT contain control characters.
+	// @see https://tools.ietf.org/html/rfc5234#appendix-B.1 (=> "CTL = %x00-1F / %x7F")
+	if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
+		return {
+			status: 401,
+			message: 'Unauthorized',
+			statusText: 'Unauthorized',
+			user,
+			pass,
+		}
+	}
+	user = decoded.substring(0, index) ? decoded.substring(0, index).toLowerCase() : '';
+	pass = decoded.substring(index + 1);
+
+	if( ! ALLOWED_USERS.includes(user) ) {
+		return {
+			status: 400,
+			message: 'Not Josh',
+			statusText: 'Unauthorized',
+			user: null,
+			pass: null,
+		}
+	}
+	return {
+	  user,
+	  pass,
+	  status: 200,
+	  message: 'Has auth'
+	};
+  }
+
+  async function basicAuthToBskyAgent(request:Request,env:Env) {
+	const {user,pass,status,message} = await basicAuthentication(request);
+	if( ! user || ! pass ) {
+		return {
+			status,
+			message,
+			agent: null,
+			resumed: false,
+		};
+	}
+	try {
+
+		const {agent,resumed} = await login({
+			identifier: user,
+			password: pass,
+			kv: env.KV,
+
+		});
+		return {
+			status,
+			message,
+			agent,
+			resumed,
+		};
+	} catch (error:any) {
+		return {
+			status: error.status ?? 400,
+			agent: null,
+			resumed: false,
+			message: error.message ? error.message : error.toString(),
+		}
+	}
+  }
+
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		if( !env.josh412bskypass ) {
+		const {
+			status,
+			message,
+			agent,
+			resumed,
+		} = await basicAuthToBskyAgent(request,env);
+		if( ! agent ) {
+			const rStatus = status >= 400 ? status : 401;
 			return jsonResponse({
-				error: 'no josh412bskypass',
+				message,
 			},{
-				status: '500',
+				status: rStatus.toString(),
 			});
 		}
+
+
 		try {
+			const {method} = request;
+			if( 'POST' === method ) {
+				const data = await request.json() as {
+					text: string,
+				};
+				if( ! data.text ) {
+					return jsonResponse({
+						error: 'no text',
+					},{
+						status: '400',
+					});
+				}
+				const {text} = data;
+				const rt = new RichText({ text })
+				await rt.detectFacets(agent)
+				const postRecord = {
+					$type: 'app.bsky.feed.post',
+					text: rt.text,
+					facets: rt.facets,
+					createdAt: new Date().toISOString(),
+					lang: 'en'
+				};
+				const results = await agent.post(postRecord);
+				return jsonResponse({
+					uri: results.uri,
+					cid: results.cid,
+					resumed,
+					facets: rt.facets,
+				});
 
-			const {agent} = await login({
-				identifier: 'josh412.com',
-				password: env.josh412bskypass,
-				kv: env.KV,
-			});
-
-
-			const results = await agent.getActorLikes({
-				actor: 'josh412.com',
-			});
-			return jsonResponse(results);
+			}else{
+				const likes = await agent.getActorLikes({
+					actor: 'josh412.com',
+				});
+				return jsonResponse({
+					resumed,
+					likesCursor: likes.data.cursor,
+					likes: likes.data.feed,
+				});
+			}
 
 
 		} catch (error: any) {
