778f3d47485d85f650805228069910fd04779861
josh <jpollock412@gmail.com>
2023-09-26 18:31:22 -0400

    Move bluesky stuff to @social

--- packages/social/src/index.ts
+++ packages/social/src/index.ts
@@ -1,2 +1,12 @@
 export const socialTest = () => console.log('social test');
+interface Buffer extends Uint8Array {
+}
+export interface Attatchment {
+    file: Buffer;
+    description: string;
+    encoding: "image/jpg"| "image/png";
+
+}
+export type Attatchments = Attatchment[];
 export * from './mastodon';
+export * from './bluesky';
