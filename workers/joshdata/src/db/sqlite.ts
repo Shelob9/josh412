0f5138410dc5fa05878a72d86de977b71d279bbc
josh <jpollock412@gmail.com>
2023-10-05 15:24:18 -0400

    remove sqlite

--- workers/joshdata/src/db/sqlite.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
-import { TABLE_classifications } from './schema';
-const sqlite = new Database('local.db');
-const db: BetterSQLite3Database = drizzle(sqlite);
