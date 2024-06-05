8e2c2f80fa61b053d00318149403c5a79eae8c29
josh <jpollock412@gmail.com>
2024-06-05 17:09:24 -0400

    Impliment wordpress proxy

--- workers/wordpress-proxy/src/index.ts
+++ workers/wordpress-proxy/src/index.ts
@@ -1,18 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const cacheSeconds = 604800
+const uri = `https://josh412.com`;
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+         // Determine which group this request is in.
+         const cookie = request.headers.get('Cookie')
+         if (cookie
+           && (
+             cookie.includes(`wordpress_logged`)
+             || cookie.includes(`comment_`)
+             || cookie.includes(`wordpress_sec`)
+           )) {
+           const bustedRequest = new Request(request, { cf: { cacheTtl: -1 } })
+           const response = await fetch(bustedRequest)
+
+           const newHeaders = new Headers(response.headers)
+           newHeaders.append('wp-cache-busted', `true`)
+           newHeaders.append('x-proxy', 'true');
+           return new Response(response.body, {
+                status: response.status,
+                statusText: response.statusText,
+                headers: newHeaders
+           })
+         } else {
+            console.log('served from Cached', request.url);
+
+            const originalUrl = new URL(request.url);
+            const newUrl = new URL(`${uri}${originalUrl.pathname}`);
+            // Edge Cache for 7 days
+               return fetch(newUrl, {
+                cf: { cacheTtl: cacheSeconds },
+                headers: {
+                    ...request.headers,
+                    'x-proxy': 'true'
+                },
+
+             })
+         };
+    },
 };
