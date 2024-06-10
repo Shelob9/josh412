a5b26a6232b5f863b79bd63d704ee84dd0907022
josh <jpollock412@gmail.com>
2024-06-10 17:28:53 -0400

    add wp-start worker

--- workers/wordpress-proxy/src/index.ts
+++ workers/wordpress-proxy/src/index.ts
@@ -1,35 +1,44 @@
 import { isAllowed, isAuthed } from "@lib/allowed";
 import config from "@lib/config";
+import { withWorkerName } from "@lib/ResponseFactory";
 const { cacheSeconds, uri } = config;
+const workerName = 'wordpress-proxy';
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
          // Determine which group this request is in.
          const cookie = request.headers.get('Cookie')
          if (isAuthed(cookie)) {
-           const bustedRequest = new Request(request, { cf: { cacheTtl: -1 } })
-           const response = await fetch(bustedRequest)
-
-           const newHeaders = new Headers(response.headers)
-           newHeaders.append('wp-cache-busted', `true`)
-           newHeaders.append('x-proxy', 'true');
-           return new Response(response.body, {
-                status: response.status,
-                statusText: response.statusText,
-                headers: newHeaders
-           })
+            const bustedRequest = new Request(request, { cf: { cacheTtl: -1 } })
+            const response = await fetch(bustedRequest)
+            const newHeaders = new Headers(response.headers)
+            newHeaders.append('wp-cache-busted', `true`)
+            newHeaders.append('x-proxy', 'true');
+            return withWorkerName( {
+                response: new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders
+                }),
+                workerName
+            })
          } else {
             const originalUrl = new URL(request.url);
             const allowed = isAllowed(originalUrl);
             const newUrl = allowed ? new URL(`${uri}${originalUrl.pathname}`) : new URL(`${uri}/404`);
-            // Edge Cache for 7 days
-               return fetch(newUrl, {
+            const response = await fetch(newUrl, {
                 cf: { cacheTtl: cacheSeconds },
                 headers: {
                     ...request.headers,
-                    'x-proxy': 'true'
                 },
-
-             })
-         };
+           });
+            return  withWorkerName( {
+                response: new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: response.headers
+                }),
+                workerName: 'wordpress-proxy'
+            })
+        };
     },
 };
---
+++ workers/wp-star/src/index.ts
@@ -0,0 +1,48 @@
+import config from '@lib/config';
+import { withWorkerName } from '@lib/ResponseFactory';
+const workerName = 'wp-star';
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		// Instantiate new URL to make it mutable
+		const originalUrl = new URL(request.url)
+		const newUrl = new URL(`${config.uri}${originalUrl.pathname}${originalUrl.search}`)
+		const queryCacheKey = `${originalUrl.hostname}${originalUrl.pathname}${originalUrl.search}`
+
+		// Different asset types usually have different caching strategies. Most of the time media content such as audio, videos and images that are not user-generated content would not need to be updated often so a long TTL would be best. However, with HLS streaming, manifest files usually are set with short TTLs so that playback will not be affected, as this files contain the data that the player would need. By setting each caching strategy for categories of asset types in an object within an array, you can solve complex needs when it comes to media content for your application
+
+		const cacheAssets = [
+			{asset: 'image', key: queryCacheKey, regex: /(.*\/Images)|(.*\.(jpg|jpeg|png|bmp|pict|tif|tiff|webp|gif|heif|exif|bat|bpg|ppm|pgn|pbm|pnm))/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
+			{asset: 'frontEnd', key: queryCacheKey, regex: /^.*\.(css|js)/, info: 0, ok: 3600, redirects: 30, clientError: 10, serverError: 0 },
+		];
+		const { asset, regex, ...cache } = cacheAssets.find( ({regex}) => originalUrl.pathname.match(regex)) ?? {}
+		const newResponse = await fetch(newUrl,
+				{ cf:
+					{
+						cacheKey: cache ? cache.key : queryCacheKey,
+						polish: false,
+						cacheEverything: true,
+						cacheTtlByStatus: {
+							'100-199': cache.info,
+							'200-299': cache.ok,
+							'300-399': cache.redirects,
+							'400-499': cache.clientError,
+							'500-599': cache.serverError
+							},
+						cacheTags: [
+							'static'
+							]
+					},
+
+				})
+				console.log(cache);
+
+		const response = new Response(newResponse.body, newResponse)
+
+		// For debugging purposes
+		response.headers.set('debug', JSON.stringify(cache))
+		return withWorkerName( {
+			response,
+			workerName
+		})
+	},
+};
