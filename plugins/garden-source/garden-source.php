b3b19307993750f0991c0a151cf99e63616dce9a
josh <jpollock412@gmail.com>
2024-10-21 17:28:35 -0400

    Plugin  0.5.4

--- plugins/garden-source/garden-source.php
+++ plugins/garden-source/garden-source.php
@@ -4,7 +4,7 @@
  * Description:
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.5.3
+ * Version:           0.5.4
  * Author:            Josh Pollock
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
